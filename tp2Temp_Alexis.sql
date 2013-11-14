e3127b1e5bbee30c27e553115cf9afe0045c66e2
Pieplu <alexis.pieplu@gmail.com>
2013-11-13 19:02:32 -0500

    c2 en test

--- tp2Temp_Alexis.sql
+++ tp2Temp_Alexis.sql
@@ -1,3 +1,7 @@
 -- C1
-alter table professeur
-add constraint checkCodeProf check(REGEXP_LIKE(codeProfesseur, '[A-Z]{4}[0-9]'));
+ALTER TABLE professeur
+ADD CONSTRAINT checkCodeProf CHECK(REGEXP_LIKE(codeProfesseur, '[A-Z]{4}[0-9]')); --OK
+
+--C2
+ALTER TABLE inscription
+ADD CONSTRAINT checkNote CHECK(note >= 0 AND note <= 100);
